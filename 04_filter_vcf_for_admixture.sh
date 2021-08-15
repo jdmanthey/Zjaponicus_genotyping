75656feb21911a6f8f3f234b114e066796d08f6a
Joe Manthey <jdmanthey@gmail.com>
2021-08-15 09:27:39 -0500

    Update 04_filter_vcf_for_admixture.sh

--- 04_filter_vcf_for_admixture.sh
+++ 04_filter_vcf_for_admixture.sh
@@ -13,5 +13,5 @@ input_array=$( head -n${SLURM_ARRAY_TASK_ID} helper6.txt | tail -n1 )
 # define main working directory
 workdir=/lustre/scratch/jmanthey/38_zjaponicus
 
-# run vcftools with SNP output spaced 10kbp
-vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf --remove-indv Zosterops_japonicus_Guizhou_11220 --max-missing 1.0 --minQ 20 --minGQ 20 --minDP 6 --max-meanDP 50 --min-alleles 2 --max-alleles 2 --mac 1 --thin 10000 --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/04_filtered_vcf_10kbp/${input_array}
+# run vcftools with SNP output spaced 1kbp
+vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf --remove-indv Zosterops_japonicus_Guizhou_11220 --max-missing 1.0 --minQ 20 --minGQ 20 --minDP 6 --max-meanDP 50 --min-alleles 2 --max-alleles 2 --mac 1 --thin 1000 --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/04_filtered_vcf_1kbp/${input_array}
