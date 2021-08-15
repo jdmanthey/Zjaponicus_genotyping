57ed614fe28773a423886e0c7139780d20880bfe
Joe Manthey <jdmanthey@gmail.com>
2021-08-15 08:49:51 -0500

    Create 04_filter_vcf_for_admixture.sh

---
+++ 04_filter_vcf_for_admixture.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=filter
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH --partition quanah
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-120
+
+# define input files from helper file during genotyping
+input_array=$( head -n${SLURM_ARRAY_TASK_ID} helper6.txt | tail -n1 )
+
+# define main working directory
+workdir=/lustre/scratch/jmanthey/38_zjaponicus
+
+# run vcftools with SNP output spaced 10kbp
+vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf --max-missing 1.0 --minQ 20 --minGQ 20 --minDP 6 --max-meanDP 50 --min-alleles 2 --max-alleles 2 --mac 1 --thin 10000 --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/04_filtered_vcf_10kbp/${input_array}
