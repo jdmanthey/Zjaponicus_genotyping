ddc36497e3b1a6c01820a848ca7b0e33aed3fc86
Joe Manthey <jdmanthey@gmail.com>
2024-09-06 17:11:55 -0500

    Update 01_setup.sh

--- 01_setup.sh
+++ 01_setup.sh
@@ -1,23 +1,33 @@
-# move all fastq to current directory
-find . -name '*fastq.gz' -print0 | xargs -0 -I '{}' mv "{}" .
+# define main working directory
+workdir=/lustre/scratch/jmanthey/02_zj/
 
-# rename the files
-while read -r name1 name2; do
-	mv $name1 $name2
-done < rename_fq.txt
+# make output directories
+cd ${workdir}
 
-# make sure the reference genome is indexed
-cd /lustre/work/jmanthey/zosterops_genome/
-samtools faidx ref.fa
-bwa index ref.fa
-cd ~
-java -jar picard.jar CreateSequenceDictionary R=/lustre/work/jmanthey/zosterops_genome/ref.fa O=/lustre/work/jmanthey/zosterops_genome/ref.dict
-
-# make directories for organization during genotyping
 mkdir 00_fastq
 mkdir 01_cleaned
-mkdir 01_mtDNA
 mkdir 01_bam_files
 mkdir 02_vcf
 mkdir 03_vcf
-mkdir 10_align_script
+mkdir 04_vcf
+mkdir 05_pca
+mkdir 06_window
+mkdir 06_window/windows
+mkdir 10_filter
+mkdir 11_filter_vcf
+
+
+# index the reference for several programs
+interactive -p nocona -c 4 -m 8G
+
+cd references
+
+source activate bcftools
+
+bwa-mem2 index GCA_017612475.1_CCG_JAWE_1.0_genomic.fna
+
+samtools faidx GCA_017612475.1_CCG_JAWE_1.0_genomic.fna
+
+java -jar picard.jar CreateSequenceDictionary \
+R=/home/jmanthey/references/GCA_017612475.1_CCG_JAWE_1.0_genomic.fna \
+O=/home/jmanthey/references/GCA_017612475.1_CCG_JAWE_1.0_genomic.dict
